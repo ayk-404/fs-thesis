aa17df0a75a7eae63ee8f34d7a4ed94ec14d41cc
Andrey Kudryavtsev <96312101+hackingbandy@users.noreply.github.com>
2026-02-15 14:41:54 +0100

    add todo

--- tests/test_data.py
+++ tests/test_data.py
@@ -1,10 +1,16 @@
 import unittest
 
+from duckdb import df
+
 
 class TestCodeIsTested(unittest.TestCase):
 
     def test_code_is_tested(self):
         self.assertTrue(False)
+
+    def test_features(self):
+        # TODO: Testen ob die Daten korrekt geladen und verarbeitet werden
+
 
 
 if __name__ == '__main__':
