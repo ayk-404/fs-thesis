1ef51736f340e2543dc27257f1234822c3380477
Andrey Kudryavtsev <96312101+hackingbandy@users.noreply.github.com>
2026-04-22 21:35:24 +0200

    quick review

---
+++ notebooks/EDA/features.py
@@ -0,0 +1,29 @@
+# Imports
+import sys
+sys.path.insert(0, '..')
+import polars as pl
+import time
+import json
+import warnings
+import numpy as np
+import pandas as pd
+from pathlib import Path
+from datetime import datetime
+from tqdm.auto import tqdm
+
+import plotly.express as px
+import plotly.graph_objects as go
+import plotly.figure_factory as ff
+
+from sklearn.metrics import (
+    accuracy_score, f1_score, roc_auc_score, recall_score, precision_score,
+    classification_report
+)
+
+from fs_thesis.data_loader import load_final_data
+from fs_thesis.preprocessing import preprocess_data, balance_data, get_X_y
+
+warnings.filterwarnings('ignore')
+
+df = load_final_data()
+print(df.head())
